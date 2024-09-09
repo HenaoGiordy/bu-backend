73d82efbd380485d0997e15f4c8c7c4d3f214f14
Giordy <henaogiordy@gmail.com>
2024-09-08 20:04:01 -0500

    First Commint

---
+++ src/main/java/com/univalle/bubackend/BuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.univalle.bubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuBackendApplication.class, args);
+    }
+
+}
