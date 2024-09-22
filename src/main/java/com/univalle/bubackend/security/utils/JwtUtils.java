a24a22633ad4427751504f2f85bade8a5f7a6777
Giordy <henaogiordy@gmail.com>
2024-09-22 09:15:46 -0500

    Deleting Imports

--- src/main/java/com/univalle/bubackend/security/utils/JwtUtils.java
+++ src/main/java/com/univalle/bubackend/security/utils/JwtUtils.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
