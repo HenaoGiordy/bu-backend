5aaaea890a83baf1ee366f57b202ffd32e01cdcc
Giordy <henaogiordy@gmail.com>
2024-10-08 21:25:04 -0500

    modelo AppointmentReservation

---
+++ src/main/java/com/univalle/bubackend/models/AppointmentReservation.java
@@ -0,0 +1,25 @@
+package com.univalle.bubackend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AppointmentReservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "availabledates_id")
+    private AvailableDates availableDates;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "estudiante_id")
+    private UserEntity estudiante;
+}
