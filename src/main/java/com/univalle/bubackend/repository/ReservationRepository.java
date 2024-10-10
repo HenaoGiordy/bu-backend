cd246fbd2d28835b4167f908380df441e61c28e5
Jaider-Dev <jaiderbermudez.contact@gmail.com>
2024-10-09 19:16:31 -0500

    fix

--- src/main/java/com/univalle/bubackend/repository/ReservationRepository.java
+++ src/main/java/com/univalle/bubackend/repository/ReservationRepository.java
@@ -3,6 +3,7 @@ package com.univalle.bubackend.repository;
 import com.univalle.bubackend.models.Reservation;
 import com.univalle.bubackend.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -11,7 +12,9 @@ import java.util.List;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 
+    @Query("SELECT COUNT(r) FROM Reservation r WHERE r.data = :date AND r.lunch = :isLunch")
     int countRemainingSlotsForDay(LocalDate date, boolean isLunch);
 
+    @Query("SELECT r FROM Reservation r WHERE r.userEntity = :userEntity AND r.paid = false")
     List<Reservation> findByUserEntityAndPaidFalse(UserEntity userEntity);
 }
