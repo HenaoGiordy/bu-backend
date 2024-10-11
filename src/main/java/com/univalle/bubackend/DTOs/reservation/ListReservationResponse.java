62edf341e0e452f5bd5285a357f3909c325a1f5c
Jaider-Dev <jaiderbermudez.contact@gmail.com>
2024-10-11 18:29:55 -0500

    Implementación de la paginación para las reservas no pagadas

---
+++ src/main/java/com/univalle/bubackend/DTOs/reservation/ListReservationResponse.java
@@ -0,0 +1,18 @@
+package com.univalle.bubackend.DTOs.reservation;
+
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Builder
+public record ListReservationResponse(
+        Integer id,
+        LocalDateTime data,
+        LocalTime time,
+        Boolean paid,
+        Boolean snack,
+        Boolean lunch,
+        String username
+) {
+}
--- src/main/java/com/univalle/bubackend/services/reservation/IReservationService.java
+++ src/main/java/com/univalle/bubackend/services/reservation/IReservationService.java
@@ -2,8 +2,11 @@ package com.univalle.bubackend.services.reservation;
 
 import com.univalle.bubackend.DTOs.payment.ReservationPaymentRequest;
 import com.univalle.bubackend.DTOs.payment.ReservationPaymentResponse;
+import com.univalle.bubackend.DTOs.reservation.ListReservationResponse;
 import com.univalle.bubackend.DTOs.reservation.ReservationRequest;
 import com.univalle.bubackend.DTOs.reservation.ReservationResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -12,5 +15,5 @@ public interface IReservationService {
     ReservationResponse cancelReservation(Integer reservationId);
     ReservationResponse findReservationByUsername(String username);
     ReservationPaymentResponse registerPayment(ReservationPaymentRequest paymentRequest);
-    List<ReservationResponse> getActiveReservations();
+    Page<ListReservationResponse> getActiveReservations(Pageable pageable);
 }
