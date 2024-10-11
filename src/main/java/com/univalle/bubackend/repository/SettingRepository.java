45465c7870c8cafbdbf51650b374036d767e3888
Jaider-Dev <jaiderbermudez.contact@gmail.com>
2024-10-11 10:08:48 -0500

    Implementación del get para visualizar perfil (ajustes)

--- src/main/java/com/univalle/bubackend/repository/SettingRepository.java
+++ src/main/java/com/univalle/bubackend/repository/SettingRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface SettingRepository extends JpaRepository<Setting, Integer> {
     Optional<Setting> findTopByOrderByIdAsc();
+    Optional<Setting> findSettingById(Integer id);
 }
