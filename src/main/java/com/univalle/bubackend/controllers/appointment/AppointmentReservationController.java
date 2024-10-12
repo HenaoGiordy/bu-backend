7665e7f75a73b66bfc197e82acec350c8b5766c2
Giordy <henaogiordy@gmail.com>
2024-10-12 11:51:37 -0500

    Documentation

--- src/main/java/com/univalle/bubackend/controllers/appointment/AppointmentReservationController.java
+++ src/main/java/com/univalle/bubackend/controllers/appointment/AppointmentReservationController.java
@@ -1,16 +1,20 @@
 package com.univalle.bubackend.controllers.appointment;
 
-import com.univalle.bubackend.DTOs.appointment.RequestAppointmentReservation;
-import com.univalle.bubackend.DTOs.appointment.ResponseAppointmentReservation;
-import com.univalle.bubackend.DTOs.appointment.ResponseAppointmentReservationProfessional;
-import com.univalle.bubackend.DTOs.appointment.ResponseAppointmentReservationStudent;
+import com.univalle.bubackend.DTOs.appointment.*;
 import com.univalle.bubackend.services.appointment.reservation.IAppointmentReservationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @AllArgsConstructor
@@ -20,16 +24,81 @@ public class AppointmentReservationController {
 
     private IAppointmentReservationService appointmentReservationService;
 
+    @Operation(
+            summary = "Reservar una cita",
+            description = "Permite reservar una cita, dado un horario disponible"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cita reservada exitosamente",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseAppointmentReservation.class))}),
+            @ApiResponse(responseCode = "400", description = "Error de validación en la solicitud",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Usuario no autenticado o token inválido",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Usuario no autorizado para realizar esta operación",
+                    content = @Content)
+    })
+    @RequestBody(
+            description = "Debes Ingresar el Id del paciente y el Id del horario disponible de un profesional.",
+            required = true,
+            content = @Content(schema = @Schema(implementation = RequestAppointmentReservation.class))
+    )
     @PostMapping
     public ResponseEntity<ResponseAppointmentReservation> reservation(@Valid @RequestBody RequestAppointmentReservation requestAppointmentReservation) {
         return new ResponseEntity<>(appointmentReservationService.reserveAppointment(requestAppointmentReservation), HttpStatus.CREATED);
     }
 
+
+
+    @Operation(
+            summary = "Obtener la citas del profesional",
+            description = "Permite obtener todas la citas de un profesional con su Id"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todas las citas",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseAppointmentReservationProfessional.class))}),
+            @ApiResponse(responseCode = "400", description = "Error de validación en la solicitud",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Usuario no autenticado o token inválido",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Usuario no autorizado para realizar esta operación",
+                    content = @Content)
+
+    })
+    @Parameter(
+            name = "id",
+            description = "Debes ingresar el id del profesional del cual quieres obtener la citas ",
+            required = true,
+            example = "1"
+    )
     @GetMapping("/professional/{id}")
-    public ResponseEntity<ResponseAppointmentReservationProfessional> getAppointmentById(@PathVariable Integer id) {
+    public ResponseEntity<ResponseAppointmentReservationProfessional> getAppointmentById( @PathVariable Integer id) {
         return new ResponseEntity<>(appointmentReservationService.allAppointmentProfessional(id), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Obtener la citas del estudiante",
+            description = "Permite obtener todas la citas de un estudiante con su Id"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todas las citas",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseAppointmentReservationStudent.class))}),
+            @ApiResponse(responseCode = "400", description = "Error de validación en la solicitud",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "Usuario no autenticado o token inválido",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Usuario no autorizado para realizar esta operación",
+                    content = @Content)
+    })
+    @Parameter(
+            name = "id",
+            description = "Debes ingresar el Id del estudiante del cual quieres obtener las citas",
+            required = true,
+            example = "1"
+    )
     @GetMapping("/student/{id}")
     public ResponseEntity<ResponseAppointmentReservationStudent> getAppointmentByAppointmentId(@PathVariable Integer id) {
         return new ResponseEntity<>(appointmentReservationService.allAppointmentEstudiante(id), HttpStatus.OK);
