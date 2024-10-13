95ec21a6f073ff32d038abd381f664edec920a39
Jaider-Dev <jaiderbermudez.contact@gmail.com>
2024-10-12 22:21:20 -0500

    Actualización de la consulta de las disponibilidad de almuerzos y refrigerios disponibles por día

--- src/main/java/com/univalle/bubackend/repository/ReservationRepository.java
+++ src/main/java/com/univalle/bubackend/repository/ReservationRepository.java
@@ -14,24 +14,26 @@ import java.util.List;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 
-    @Query("SELECT COUNT(r) FROM Reservation r WHERE DATE(r.data) = :date AND r.lunch = :isLunch")
-    int countRemainingLunchSlotsForDay(LocalDate date, boolean isLunch);
+    @Query("SELECT s.numLunch FROM Setting s")
+    int getMaxRemainingLunchSlots();
 
-    @Query("SELECT COUNT(r) FROM Reservation r WHERE DATE(r.data) = :date AND r.snack = :isSnack")
-    int countRemainingSnacklotsForDay(LocalDate date, boolean isSnack);
+    @Query("SELECT s.numSnack FROM Setting s")
+    int getMaxRemainingSnackSlots();
+
+    @Query("SELECT COUNT(r) FROM Reservation r WHERE r.lunch = TRUE AND r.data = :today")
+    int countLunchReservationsForDay(LocalDate today);
+
+    @Query("SELECT COUNT(r) FROM Reservation r WHERE r.snack = TRUE AND r.data = :today")
+    int countSnackReservationsForDay(LocalDate today);
 
     @Query("SELECT r FROM Reservation r WHERE r.userEntity = :userEntity AND r.paid = false")
     List<Reservation> findByUserEntityAndPaidFalse(UserEntity userEntity);
 
-
-    @Query("SELECT r FROM Reservation r WHERE r.paid = false AND DATE(r.data) = :date")
-    List<Reservation> findAllByPaidFalse(LocalDate date);
-
     @Query("SELECT r FROM Reservation r Where r.userEntity = :userEntity AND DATE(r.data) = :date")
     List<Reservation> findReservationsPerDay(UserEntity userEntity, LocalDate date);
 
-    @Query("SELECT r FROM Reservation r WHERE r.paid = false")
-    Page<Reservation> findAllByPaidFalse(Pageable pageable);
+    @Query("SELECT r FROM Reservation r WHERE r.paid = false AND DATE(r.data) = :date")
+    Page<Reservation> findAllByPaidFalse(Pageable pageable, LocalDate date);
 
 
 }
