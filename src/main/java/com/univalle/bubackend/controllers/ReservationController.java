77ca9d662f17534c2f896748510180bd07ac8025
Jaider-Dev <jaiderbermudez.contact@gmail.com>
2024-11-11 19:20:00 -0500

    manejo de excepciones para reserva

--- src/main/java/com/univalle/bubackend/controllers/ReservationController.java
+++ src/main/java/com/univalle/bubackend/controllers/ReservationController.java
@@ -3,20 +3,24 @@ package com.univalle.bubackend.controllers;
 import com.univalle.bubackend.DTOs.payment.ReservationPaymentRequest;
 import com.univalle.bubackend.DTOs.payment.ReservationPaymentResponse;
 import com.univalle.bubackend.DTOs.reservation.*;
+import com.univalle.bubackend.exceptions.ResourceNotFoundException;
+import com.univalle.bubackend.exceptions.reservation.NoSlotsAvailableException;
+import com.univalle.bubackend.exceptions.reservation.UnauthorizedException;
 import com.univalle.bubackend.services.reservation.IReservationService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
 
 import java.util.Collections;
 import java.util.List;
@@ -27,149 +31,140 @@ import java.util.List;
 public class ReservationController {
 
     private final IReservationService reservationService;
-    @Operation(summary = "Crear reserva de estudiante", description = "Crea una nueva reserva para un estudiante.")
+
+    @Operation(summary = "Create a reservation for a student")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reserva creada exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta", content = @Content)
+            @ApiResponse(responseCode = "201", description = "Reservation created successfully",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Resource not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Unauthorized to make reservation",
+                    content = @Content),
+            @ApiResponse(responseCode = "409", description = "No slots available",
+                    content = @Content)
     })
     @PostMapping("/create")
-    public ResponseEntity<ReservationResponse> createStudentReservation(@Valid @RequestBody ReservationStudentRequest request) {
+    public ResponseEntity<?> createStudentReservation(@Valid @RequestBody ReservationStudentRequest request) {
         try {
             ReservationResponse response = reservationService.createStudentReservation(request);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (UnauthorizedException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        } catch (NoSlotsAvailableException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }
     }
 
-    @Operation(summary = "Crear reserva de externo", description = "Crea una nueva reserva para un usuario externo.")
+    @Operation(summary = "Create a reservation for an external user")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reserva creada exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta", content = @Content)
+            @ApiResponse(responseCode = "201", description = "Reservation created successfully",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Resource not found",
+                    content = @Content),
+            @ApiResponse(responseCode = "403", description = "Unauthorized to make reservation",
+                    content = @Content),
+            @ApiResponse(responseCode = "409", description = "No slots available",
+                    content = @Content)
     })
     @PostMapping("/create-extern")
-    public ResponseEntity<ReservationResponse> createExternReservation(@Valid @RequestBody ReservationExternRequest request) {
+    public ResponseEntity<?> createExternReservation(@Valid @RequestBody ReservationExternRequest request) {
         try {
             ReservationResponse response = reservationService.createExternReservation(request);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (UnauthorizedException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        } catch (NoSlotsAvailableException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
         }
     }
 
-
-    @Operation(summary = "Buscar reserva de externo por username", description = "Obtiene la reserva de un externo por su username.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reserva encontrada", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExternResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Reserva no encontrada", content = @Content)
-    })
-    @GetMapping("/extern/{username}")
-    public ResponseEntity<ExternResponse> findExternUsername(@PathVariable String username) {
-        try {
-            ExternResponse responses = reservationService.getExtern(username);
-            return ResponseEntity.ok(responses);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    @Operation(summary = "Get availability for reservations")
+    @ApiResponse(responseCode = "200", description = "Availability fetched successfully",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AvailabilityResponse.class)))
+    @GetMapping("/availability")
+    public ResponseEntity<AvailabilityResponse> getAvailability() {
+        AvailabilityResponse availability = reservationService.getAvailability();
+        return ResponseEntity.ok(availability);
     }
 
-    @Operation(summary = "Obtener reservas por día", description = "Obtiene las reservas de un usuario para un día específico.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reservas encontradas", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Usuario no encontrado", content = @Content)
-    })
-    @GetMapping("/per-day/{username}")
-    public ResponseEntity<List<ReservationResponse>> getReservationsPerDay(@PathVariable String username) {
-        try {
-            List<ReservationResponse> responses = reservationService.getReservationsPerDay(username);
-            return ResponseEntity.ok(responses);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+    @Operation(summary = "Get availability per hour for reservations")
+    @ApiResponse(responseCode = "200", description = "Availability per hour fetched successfully",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = AvailabilityPerHourResponse.class)))
+    @GetMapping("/availability-per-hour")
+    public ResponseEntity<AvailabilityPerHourResponse> getAvailabilityPerHour() {
+        AvailabilityPerHourResponse availability = reservationService.getAvailabilityPerHour();
+        return ResponseEntity.ok(availability);
     }
 
-    @Operation(summary = "Cancelar reserva", description = "Cancela una reserva existente por su ID.")
+    @Operation(summary = "Get reservations by username")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reserva cancelada exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Reserva no encontrada", content = @Content)
-    })
-    @DeleteMapping("/cancel/{id}")
-    public ResponseEntity<ReservationResponse> cancelReservation(@PathVariable Integer id) {
-        try {
-            ReservationResponse response = reservationService.cancelReservation(id);
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-    }
-
-
-    @Operation(summary = "Buscar reservas por username", description = "Obtiene todas las reservas de un usuario por su username.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reservas encontradas", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
-            @ApiResponse(responseCode = "404", description = "Usuario no encontrado", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Reservations found",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content)
     })
     @GetMapping("/by-username/{username}")
-    public ResponseEntity<List<ReservationResponse>> findReservationByUsername(@PathVariable String username) {
+    public ResponseEntity<?> findReservationByUsername(@PathVariable String username) {
         try {
             List<ReservationResponse> responses = Collections.singletonList(reservationService.findReservationByUsername(username));
             return ResponseEntity.ok(responses);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 
-
-    @Operation(summary = "Registrar pago de reserva", description = "Registra el pago de una reserva existente.")
+    @Operation(summary = "Register payment for reservation")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Pago registrado exitosamente", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationPaymentResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Solicitud incorrecta", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Payment registered successfully",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationPaymentResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad request",
+                    content = @Content)
     })
     @PutMapping("/register-payment")
-    public ResponseEntity<ReservationPaymentResponse> registerPayment(@RequestBody ReservationPaymentRequest paymentRequest) {
+    public ResponseEntity<?> registerPayment(@RequestBody ReservationPaymentRequest paymentRequest) {
         try {
             ReservationPaymentResponse response = reservationService.registerPayment(paymentRequest);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
-    @Operation(summary = "Obtener todas las reservas", description = "Obtiene todas las reservas activas con paginación.")
+    @Operation(summary = "Get all active reservations with pagination")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reservas encontradas", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListReservationResponse.class))),
-            @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Reservations found",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ListReservationResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
     })
     @GetMapping("/all")
-    public ResponseEntity<Page<ListReservationResponse>> getAllReservations(
-            @PageableDefault(size = 10, page = 0) Pageable pageable) {
+    public ResponseEntity<?> getAllReservations(@PageableDefault(size = 10, page = 0) Pageable pageable) {
         try {
             Page<ListReservationResponse> reservations = reservationService.getActiveReservations(pageable);
-            return new ResponseEntity<>(reservations, HttpStatus.OK);
+            return ResponseEntity.ok(reservations);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
     }
 
-
-    @Operation(summary = "Obtener disponibilidad", description = "Obtiene la disponibilidad para reservas de almuerzo y refrigerio.")
+    @Operation(summary = "Cancel a reservation")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Disponibilidad encontrada", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AvailabilityResponse.class)))
+            @ApiResponse(responseCode = "200", description = "Reservation canceled successfully",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReservationResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Reservation not found",
+                    content = @Content)
     })
-    @GetMapping("/availability")
-    public ResponseEntity<AvailabilityResponse> getAvailability() {
-        AvailabilityResponse responses = reservationService.getAvailability();
-        return ResponseEntity.ok(responses);
+    @DeleteMapping("/cancel/{id}")
+    public ResponseEntity<?> cancelReservation(@PathVariable Integer id) {
+        try {
+            ReservationResponse response = reservationService.cancelReservation(id);
+            return ResponseEntity.ok(response);
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
-
-
-    @Operation(summary = "Obtener disponibilidad por hora", description = "Obtiene la disponibilidad de reservas por hora.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Disponibilidad por hora encontrada", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AvailabilityPerHourResponse.class)))
-    })
-    @GetMapping("/availability-per-hour")
-    public AvailabilityPerHourResponse getAvailabilityPerHour() {
-        return reservationService.getAvailabilityPerHour();
-    }
-
 }
