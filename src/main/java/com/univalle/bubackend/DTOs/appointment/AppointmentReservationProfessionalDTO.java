e80d811773491e147d017394f8289a3ee455e11d
Giordy <henaogiordy@gmail.com>
2024-11-17 13:27:52 -0500

    Correción método all dates

--- src/main/java/com/univalle/bubackend/DTOs/appointment/AppointmentReservationProfessionalDTO.java
+++ src/main/java/com/univalle/bubackend/DTOs/appointment/AppointmentReservationProfessionalDTO.java
@@ -3,12 +3,13 @@ package com.univalle.bubackend.DTOs.appointment;
 import com.univalle.bubackend.DTOs.user.UserEntityDTO;
 import com.univalle.bubackend.models.AppointmentReservation;
 
-public record AppointmentReservationProfessionalDTO(Integer reservationId, String patient, Long phone, AvailableDateDTO availableDate, UserEntityDTO professional, Boolean assitant, Boolean pending) {
+public record AppointmentReservationProfessionalDTO(Integer reservationId, String patient, String patientLastname, Long phone, AvailableDateDTO availableDate, UserEntityDTO professional, Boolean assitant, Boolean pending) {
 
     public AppointmentReservationProfessionalDTO(AppointmentReservation appointmentReservation) {
         this(
                 appointmentReservation.getId(),
                 appointmentReservation.getEstudiante().getName(),
+                appointmentReservation.getEstudiante().getLastName(),
                 appointmentReservation.getEstudiante().getPhone(),
                 new AvailableDateDTO(appointmentReservation.getAvailableDates()),
                 new UserEntityDTO(appointmentReservation.getAvailableDates().getProfessional()),
