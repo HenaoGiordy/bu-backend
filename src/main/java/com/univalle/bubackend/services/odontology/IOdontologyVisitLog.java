55f47ad42b80a8c1337e92fc1844a328021b2b31
karol arias <karol.ospina@correounivalle.edu.co>
2024-11-22 19:30:05 -0500

    visit odontology report

--- src/main/java/com/univalle/bubackend/services/odontology/IOdontologyVisitLog.java
+++ src/main/java/com/univalle/bubackend/services/odontology/IOdontologyVisitLog.java
@@ -1,9 +1,12 @@
 package com.univalle.bubackend.services.odontology;
 
 import com.univalle.bubackend.DTOs.odontology.*;
+import com.univalle.bubackend.models.VisitOdontologyLog;
 import org.springframework.data.domain.Pageable;
 
+import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
+import java.util.List;
 
 public interface IOdontologyVisitLog {
     UserResponse findStudentsByUsername(String username);
@@ -13,4 +16,6 @@ public interface IOdontologyVisitLog {
     VisitOdontologyResponse visitsOdonotology(String username, LocalDate startDate, LocalDate endDate, Pageable pageable);
 
     VisitResponse getOdontologyVisit(Long id);
+
+    ByteArrayInputStream downloadOdontologyReport();
 }
