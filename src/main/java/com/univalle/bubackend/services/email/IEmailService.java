995b40a3a09ed2259d979faeaa7bd99915322e44
Jaider-Dev <jaiderbermudez.contact@gmail.com>
2024-11-26 09:32:07 -0500

    Revert "correción en la hora de cancelación de almuerzo"

--- src/main/java/com/univalle/bubackend/services/email/IEmailService.java
+++ src/main/java/com/univalle/bubackend/services/email/IEmailService.java
@@ -2,11 +2,11 @@ package com.univalle.bubackend.services.email;
 
 import com.univalle.bubackend.models.Reservation;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public interface IEmailService {
     void sendPasswordResetEmail(String email, String token);
 
-    void sendReservationCancellationEmail(String type, Reservation reservation, LocalDate date, LocalTime time);
+    void sendReservationCancellationEmail(String type, Reservation reservation, LocalDateTime date, LocalTime time);
 }
